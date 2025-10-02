ad238678cbbda9086f3c199bef987046f36a6134
Abdullah Al Roman <abdullahr583@live.com>
2025-10-02 12:57:27 +0600

    fix up design

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -20,7 +20,7 @@ class _NewsScreenState extends State<NewsScreen> {
     return BlocProvider(
       create: (_) => NewsBloc(NewsRepository())..add(FetchNewsEvent()),
       child: Scaffold(
-        appBar: AppBar(title: const Center(child: Text('News App'))),
+        appBar: AppBar(title: const Center(child: Text('News App', style: TextStyle(fontWeight: FontWeight.w400),))),
         body: BlocBuilder<NewsBloc, NewsState>(
           builder: (context, state) {
             if (state is NewsLoading) {
@@ -30,76 +30,93 @@ class _NewsScreenState extends State<NewsScreen> {
                 onRefresh: () async {
                   context.read<NewsBloc>().add(FetchNewsEvent());
                 },
-                child: ListView.builder(
-                  itemCount: state.articles.length,
-                  itemBuilder: (_, index) {
-                    final article = state.articles[index];
-                    return GestureDetector(
-                      onTap: () {
-                        context.read<NewsBloc>().add(
-                          OpenNewsInBrowserEvent(article.url),
-                        );
-                      },
-                      child: Card(
-                        margin: const EdgeInsets.all(12.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            if (article.urlToImage != null)
-                              Image.network(article.urlToImage!)
-                            else
-                              const SizedBox(
-                                height: 200,
-                                child: Center(
-                                  child: Icon(
-                                    Icons.image,
-                                    size: 100,
-                                    color: Colors.grey,
+                child: Container(
+                  color: Colors.grey[200],
+                  child: ListView.builder(
+                    itemCount: state.articles.length,
+                    itemBuilder: (_, index) {
+                      final article = state.articles[index];
+                      return GestureDetector(
+                        onTap: () {
+                          context.read<NewsBloc>().add(
+                            OpenNewsInBrowserEvent(article.url),
+                          );
+                        },
+                        child: Card(
+                          margin: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              if (article.urlToImage != null)
+                                Container(
+                                  margin: const EdgeInsets.all(16.0),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(16),
+                                    child: Image.network(article.urlToImage!),
                                   ),
-                                ),
-                              ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                article.title,
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 8.0,
-                                  ),
-                                  child: Text(
-                                    DateFormat('dd-MMM-yyyy').format(
-                                      DateTime.parse(article.publishedAt),
+                                )
+                              else
+                                const SizedBox(
+                                  height: 200,
+                                  child: Center(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(16),
+                                      ),
+                                      child: Icon(
+                                        Icons.image,
+                                        size: 200,
+                                        color: Colors.grey,
+                                      ),
                                     ),
-                                    style: const TextStyle(color: Colors.grey),
                                   ),
                                 ),
-                                const Padding(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 8.0,
-                                  ),
-                                  child: Icon(
-                                    Icons.bookmark_border,
-                                    size: 25,
-                                    color: Colors.black54,
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  article.title,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                              ],
-                            ),
-                            const SizedBox(height: 28.0),
-                          ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 8.0,
+                                    ),
+                                    child: Text(
+                                      DateFormat('dd-MMM-yyyy').format(
+                                        DateTime.parse(article.publishedAt),
+                                      ),
+                                      style: const TextStyle(
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ),
+                                  const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 8.0,
+                                    ),
+                                    child: Icon(
+                                      Icons.bookmark_border,
+                                      size: 25,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 28.0),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
               );
             } else if (state is NewsError) {
